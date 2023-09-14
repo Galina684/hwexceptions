f9d58fadf7501c6ef0a1dac1a8efeb8161cf82c0
Galina684 <galina684@gmail.com>
2023-09-14 16:37:46 +0300

    Домашка по Исключениям

---
+++ src/test/java/pro/sky/hwexceptions/HwExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hwexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
